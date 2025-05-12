3679b9de9850b7b4f1ad42240e91fcf54e554513
RyuYamamoto <ryu.yamamoto@tier4.jp>
2025-05-12 17:10:16 +0900

    fix(autoware_ekf_localizer): use constexpr and string_view (#435)
    * fix(autoware_ekf_localizer) use constexpr and string_view
    
    Signed-off-by: RyuYamamoto <ryu.yamamoto@tier4.jp>
    
    * add std::string_view header
    
    Signed-off-by: RyuYamamoto <ryu.yamamoto@tier4.jp>
    
    * add std::string header
    
    Signed-off-by: RyuYamamoto <ryu.yamamoto@tier4.jp>
    
    ---------
    
    Signed-off-by: RyuYamamoto <ryu.yamamoto@tier4.jp>

--- localization/autoware_ekf_localizer/src/warning_message.cpp
+++ localization/autoware_ekf_localizer/src/warning_message.cpp
@@ -17,6 +17,7 @@
 #include <fmt/core.h>
 
 #include <string>
+#include <string_view>
 
 namespace autoware::ekf_localizer
 {
@@ -24,7 +25,7 @@ namespace autoware::ekf_localizer
 std::string pose_delay_step_warning_message(
   const double delay_time, const double delay_time_threshold)
 {
-  const std::string s =
+  constexpr std::string_view s =
     "Pose delay exceeds the compensation limit, ignored. "
     "delay: {:.3f}[s], limit: {:.3f}[s]";
   return fmt::format(s, delay_time, delay_time_threshold);
@@ -33,7 +34,7 @@ std::string pose_delay_step_warning_message(
 std::string twist_delay_step_warning_message(
   const double delay_time, const double delay_time_threshold)
 {
-  const std::string s =
+  constexpr std::string_view s =
     "Twist delay exceeds the compensation limit, ignored. "
     "delay: {:.3f}[s], limit: {:.3f}[s]";
   return fmt::format(s, delay_time, delay_time_threshold);
@@ -41,19 +42,21 @@ std::string twist_delay_step_warning_message(
 
 std::string pose_delay_time_warning_message(const double delay_time)
 {
-  const std::string s = "Pose time stamp is inappropriate, set delay to 0[s]. delay = {:.3f}";
+  constexpr std::string_view s =
+    "Pose time stamp is inappropriate, set delay to 0[s]. delay = {:.3f}";
   return fmt::format(s, delay_time);
 }
 
 std::string twist_delay_time_warning_message(const double delay_time)
 {
-  const std::string s = "Twist time stamp is inappropriate, set delay to 0[s]. delay = {:.3f}";
+  constexpr std::string_view s =
+    "Twist time stamp is inappropriate, set delay to 0[s]. delay = {:.3f}";
   return fmt::format(s, delay_time);
 }
 
 std::string mahalanobis_warning_message(const double distance, const double max_distance)
 {
-  const std::string s = "The Mahalanobis distance {:.4f} is over the limit {:.4f}.";
+  constexpr std::string_view s = "The Mahalanobis distance {:.4f} is over the limit {:.4f}.";
   return fmt::format(s, distance, max_distance);
 }
 
